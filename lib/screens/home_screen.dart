ba5a209905dec04ef707f86c239ddf1805dcebda
Angeles de Alba <aid2121@columbia.edu>
2023-03-06 01:18:43 -0500

    adding final screen with option to exit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_auth/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,9 +13,35 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 252, 33, 168),
+        title: const Text("Angeles's Flutter PhoneAuth App"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
       body: Center(
-        child: Text("HOMESCREEN"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: const Color.fromARGB(255, 252, 33, 168),
+              backgroundImage: NetworkImage(ap.userModel.profilePic),
+              radius: 50,
+            ),
+            const SizedBox(height: 20),
+            Text(ap.userModel.name),
+            Text(ap.userModel.phoneNumber),
+            Text(ap.userModel.email),
+            Text(ap.userModel.bio),
+          ],
+        ),
       ),
     );
   }

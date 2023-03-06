8f323802b7d3aaf657d7f103280e222aabac8d4a
Angeles de Alba <aid2121@columbia.edu>
2023-03-06 01:03:27 -0500

    saving data locally and calling firebase methods

--- lib/screens/user_info.dart
+++ lib/screens/user_info.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:phone_auth/model/user_model.dart';
 import 'package:phone_auth/provider/auth_provider.dart';
+import 'package:phone_auth/screens/home_screen.dart';
 import 'package:phone_auth/utils/utils.dart';
 import 'package:phone_auth/widgets/custom_button.dart';
 import 'package:provider/provider.dart';
@@ -97,7 +98,9 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
                         height: 50,
                         width: MediaQuery.of(context).size.width * 0.80,
                         child: CustomButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            storeData();
+                          },
                           text: "Continue",
                         ),
                       ),
@@ -119,7 +122,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
       child: TextFormField(
-        cursorColor: Color.fromARGB(255, 252, 33, 168),
+        cursorColor: const Color.fromARGB(255, 252, 33, 168),
         controller: controller,
         keyboardType: inputType,
         maxLines: maxLines,
@@ -128,7 +131,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
             margin: const EdgeInsets.all(8.0),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8.0),
-                color: Color.fromARGB(255, 252, 33, 168)),
+                color: const Color.fromARGB(255, 252, 33, 168)),
             child: Icon(
               icon,
               size: 20,
@@ -169,7 +172,17 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
           context: context,
           userModel: userModel,
           profilePic: image!,
-          onSucess: () {});
+          onSucess: () {
+            ap.saveUserDataSP().then(
+                  (value) => ap.setSignIn().then(
+                        (value) => Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomeScreen()),
+                            (route) => false),
+                      ),
+                );
+          });
     } else {
       showSnackBar(context, "Please upload your profile photo");
     }
